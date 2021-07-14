5c55cb21361156b3e9ecf58d90b1ee643be2e0a4
yaa223 <yaa223@lehigh.edu>
2021-07-14 19:52:17 +0200

    remove errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NavBar from "./components/NavBar";
-import RandomVRandom from "./components/RandomVRandom";
 import App from "./App";
 
 ReactDOM.render(
